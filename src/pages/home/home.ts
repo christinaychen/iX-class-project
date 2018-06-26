23bfa203363ee105aad3ba47508ebb643503de38
Christina Chen <cyc25>
2018-06-26 15:02:05 +0200

    changes to home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { RegistrationPage } from '../register/register';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
+
+
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+  
+  }
+  navigateToLogin() {
+    console.log("Navigating...");
+    this.navCtrl.push(LoginPage);
   }
 
+  navigateToRegister() {
+    console.log("Navigating...");
+    this.navCtrl.push(RegistrationPage);
+  }
 }
